94f5117084e6c0b52330f5fd3b5cfd103da616fa
Alessio Franceschelli <alessio@franceschelli.me>
2022-11-20 15:10:41 +0000

    Profiles merge

--- Loki/Skill.cs
+++ Loki/Skill.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Windows.Documents;
 using JetBrains.Annotations;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Loki
 {
@@ -9,16 +13,62 @@ namespace Loki
     {
         private float _level;
         private float _accumulator;
+		private double _xp;
+        private Dictionary<SkillType, double> _increments = new Dictionary<SkillType, double>
+        {
+            [SkillType.Unarmed] = 16.27228143,
+            [SkillType.Clubs] = 87.80272379,
+            [SkillType.Blocking] = 29.13766185,
+            [SkillType.Axes] = 114.7614143,
+            [SkillType.Run] = 816.5026443,
+            [SkillType.WoodCutting] = 1087.85807,
+            [SkillType.Bows] = 170.6985529,
+            [SkillType.Sneak] = 65.44530885,
+            [SkillType.Jump] = 364.4433961,
+            [SkillType.Swim] = 17.71874028,
+            [SkillType.Spears] = 1124.998332,
+            [SkillType.Knives] = 0,
+            [SkillType.Pickaxes] = 584.1558452,
+            [SkillType.Polearms] = 0,
+            [SkillType.Swords] = 0,
+        };
 
-        public Skill(SkillType type, float level, float accumulator)
+	public Skill(SkillType type, float level, float accumulator)
         {
             Type = type;
             SkillName = GetSkillName(type);
             Level = level;
             Accumulator = accumulator;
-        }
+            for (var i = 0; i <= ((int)level) - 1; i++)
+            {
+                _xp += Math.Pow(i + 1, 1.5) * 0.5 + 0.5;
+            }
+            var rest = level - (int)level;
+			var mmm = Math.Pow((int)level + 1, 1.5) * 0.5 + 0.5;
+            _xp += rest * mmm;
+            _xp += accumulator;
 
-        private static string GetSkillName(SkillType type)
+
+            var increment = _increments[type];
+            Debug.Write(increment);
+            while (increment > 0)
+            {
+                var xp = Math.Pow(Level + 1, 1.5) * 0.5 + 0.5;
+                if (increment > xp)
+                {
+                    increment -= xp;
+                    Level++;
+                }
+                else
+                {
+                    Level += (float)(increment / xp);
+                    increment = 0;
+                }
+			}
+			Debug.Write(Level);
+		}
+
+		private static string GetSkillName(SkillType type)
             =>
                 type switch
                 {
